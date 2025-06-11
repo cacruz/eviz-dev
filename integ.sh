46b0f2d2d93b9ef304dee3270a30d72349db7b2a
Carlos A. Cruz <carlos.a.cruz@nasa.gov>
2025-06-10 20:16:41 -0400

    Make script a bit more useful

--- integ.sh
+++ integ.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 function prompt() {
-
-read -p "Press Y to continue: " -n 1 -r
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo
-  return
-else
-  exit 1
-fi
+    exp="$1"
+    echo Running $exp
+    if [ "$PROMPT_ENABLED" -eq 1 ]; then
+        read -p "Press Y to run, or any other key to skip: " -n 1 -r
+        echo
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            echo "Skipping.."
+            return 1
+        fi
+    fi
+    return 0
 }
 
+
 set -e
+
+# Command-line args
+PROMPT_ENABLED=1
+if [[ "$2" == "--no-prompt" ]]; then
+    PROMPT_ENABLED=0
+fi
 
 if [[ -z "$EVIZ_CONFIG_PATH" ]]; then
     echo "Environment variable EVIZ_CONFIG_PATH must be defined"
@@ -26,38 +35,6 @@ fi
 
 os_type=$(uname)
 machine_name=$(hostname)
-
-#---------------------------------------------------
-# Use templates (in progress)
-skip=1
-
-if [ $skip -eq 1 ]; then
-
-    if [[ "$machine_name" == *"discover"* || "$machine_name" == *"borg"* ]]; then
-	export EVIZ_DATA_PATH=/discover/nobackup/ccruz/data/eviz/sample_data
-	export EVIZ_OUTPUT_PATH=/discover/nobackup/$USER/scratch/output
-    else   # Mac
-	export EVIZ_DATA_PATH=/Users/$USER/data/eviz
-	export EVIZ_OUTPUT_PATH=/Users/$USER/scratch/eviz/output_plots
-	if [ -z "$EVIZ_DATA_PATH" ]; then
-	    echo "Please set EVIZ_DATA_PATH"
-	    exit 1
-	fi
-	if [ -z "$EVIZ_OUTPUT_PATH" ]; then
-	    echo "Please set EVIZ_OUTPUT_PATH"
-	    exit 1
-	fi
-    fi
-#    files=$(find "$EVIZ_CONFIG_PATH" -name \*.yaml -print)
-
-#    for f in "${files[@]}"; do
-#	sed -i -r 's@EVIZ_DATA_PATH@'"$EVIZ_DATA_PATH"'@' "$f"
-#	sed -i -r 's@EVIZ_OUTPUT_PATH@'"$EVIZ_OUTPUT_PATH"'@' "$f"
-#    done
-
-fi
-#---------------------------------------------------
-
 
 if [ "$os_type" == "Darwin" ]; then
     echo "Running tests on Darwin OS"
@@ -80,61 +57,71 @@ fi
 
 echo "Single-plot tests"
 echo "------------------"
-echo
-# Use -f option (would override EVIZ_CONFIG_PATH)
-f_option=/Users/ccruz/projects/Eviz/config/simple/simple.yaml
-#echo "Gridded no specs"
-#python autoviz.py -s gridded -f $f_option -v 0
-#echo
 
-# Use -c option (would override EVIZ_CONFIG_PATH)
+# Use these options to override EVIZ_CONFIG_PATH
 c_option=/Users/ccruz/projects/Eviz/config
-echo "Using config -c $c_option"
-echo "Source: 'gridded' default specs"
-python autoviz.py -s gridded -c $c_option -v 0
-echo
+f_option=/Users/ccruz/projects/Eviz/config/simple/simple.yaml
 
-echo "Source: 'gridded' different (multiple) sources"
-python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_multiple.yaml -v 0
-echo
+if prompt "Gridded no specs"; then
+    python autoviz.py -s gridded -f $f_option -v 0
+    echo
+fi
 
-echo "Source: 'gridded' OpenDAP access"
-python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_opendap.yaml -v 0
-echo
+if prompt "Source: 'gridded' default specs"; then
+    python autoviz.py -s gridded -c $c_option -v 0
+    echo
+fi
 
-echo "Source: 'geos', with 4 files"
-python autoviz.py -s geos -v 0
-echo
+if prompt "Source: 'gridded' multiple sources"; then
+    python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_multiple.yaml -v 0
+    echo
+fi
 
-echo "Source: 'ccm', with 2 files"
-python autoviz.py -s ccm -v 0
-echo
+if  prompt "Source: 'gridded' OpenDAP access"; then
+    python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_opendap.yaml -v 0
+    echo
+fi
+    
+if prompt "Source: 'geos', with 4 files"; then
+    python autoviz.py -s geos -v 0
+    echo
+fi
+   
+if prompt  "Source: 'ccm', with 2 files"; then
+   python autoviz.py -s ccm -v 0
+   echo
+fi
+   
+if prompt "Source: 'ccm', 2 files and unit conversions"; then
+   python autoviz.py -s ccm -v 0 -f $EVIZ_CONFIG_PATH/ccm/ccm_multiple.yaml
+   echo
+fi
+   
+if prompt "Source: 'wrf', with 1 file (multiple 2D fields)"; then
+   python autoviz.py -s wrf -v 0
+   echo
+fi
+   
+if prompt "Source: 'wrf', GIF option"; then
+   python autoviz.py -s wrf -v 0 -f $EVIZ_CONFIG_PATH/wrf_gif.yaml
+   echo
+fi
 
-echo "Source: 'ccm', 2 files and unit conversions"
-python autoviz.py -s ccm -v 0 -f $EVIZ_CONFIG_PATH/ccm/ccm_multiple.yaml
-echo
-
-echo "Source: 'wrf', with 1 file (multiple 2D fields)"
-python autoviz.py -s wrf -v 0
-echo
-
-echo "Source: 'wrf', GIF option"
-python autoviz.py -s wrf -v 0 -f $EVIZ_CONFIG_PATH/wrf_gif.yaml
-echo
-
-
-echo "Source: 'lis', with 1 file (multiple 2D fields)"
-python autoviz.py -s lis -v 0
-echo
-
-echo "Source: 'airnow', with 12 files"
-python autoviz.py -s airnow -v 0
-echo
-
-echo "Source: 'omi', with 1 file"
-python autoviz.py -s omi -v 0
-echo
-
+if prompt  "Source: 'lis', with 1 file (multiple 2D fields)"; then
+   python autoviz.py -s lis -v 0
+   echo
+fi
+   
+if prompt "Source: 'airnow', with 12 files"; then
+   python autoviz.py -s airnow -v 0
+   echo
+fi
+   
+if prompt "Source: 'omi', with 1 file"; then
+   python autoviz.py -s omi -v 0
+   echo
+fi
+ 
 #echo "Source: mopitt, with 1 file"
 #python autoviz.py -s mopitt -v 0
 
@@ -152,40 +139,47 @@ echo
 
 echo "Comparison-plot tests"
 echo "---------------------"
-echo
-export EVIZ_CONFIG_PATH=/Users/ccruz/scratch/eviz/compare/config
-echo "Using config path:"
-echo "$EVIZ_CONFIG_PATH"
-echo
 
-echo "Source: 'ccm' vs 'ccm' (compare)"
-python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare.yaml
-echo
-
-echo "Source: 'ccm' vs 'ccm' (compare-diff 3x1)"
-python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare_3x1.yaml
-echo
-
-echo "Source: 'ccm' vs 'ccm' (compare-diff 2x2)"
-python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare_2x2.yaml
-echo
-
-echo "Source: 'ccm' vs 'ccm' (overlay)"
-python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare_overlay.yaml
-echo
-
-echo "Source: 'wrf', with 2 files (compare)"
-python autoviz.py -s wrf -v 0 -f $EVIZ_CONFIG_PATH/wrf_compare.yaml
-echo
-
-echo "Source: 'wrf', line-plot (compare)"
-python autoviz.py -s wrf -v 0 -f $EVIZ_CONFIG_PATH/wrf_xt_compare.yaml
-echo
-
-echo "Source: lis, with 2 files (compare multiple fields)"
-python autoviz.py -s lis -v 0
-echo
-
+if prompt "Source: 'ccm' vs 'ccm' (compare)"; then
+   python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare.yaml
+   echo
+fi
+   
+if prompt "Source: 'ccm' vs 'ccm' (compare-diff 3x1)"; then
+   python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare_3x1.yaml
+   echo
+fi
+   
+if prompt "Source: 'ccm' vs 'ccm' (compare-diff 2x2)"; then
+   python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare_2x2.yaml
+   echo
+fi
+   
+if prompt "Source: 'ccm' vs 'merra2' (compare-diff 3x1)"; then
+   python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm_merra2/app.yaml
+   echo
+fi
+   
+if prompt "Source: 'ccm' vs 'ccm' (overlay)"; then
+   python autoviz.py -s ccm -v 0 -f  $EVIZ_CONFIG_PATH/ccm/ccm_compare_overlay.yaml
+   echo
+fi
+   
+if prompt "Source: 'wrf', with 2 files (compare)"; then
+   python autoviz.py -s wrf -v 0 -f $EVIZ_CONFIG_PATH/wrf_compare.yaml
+   echo
+fi
+   
+if prompt "Source: 'wrf', line-plot (compare)"; then
+   python autoviz.py -s wrf -v 0 -f $EVIZ_CONFIG_PATH/wrf_xt_compare.yaml
+   echo
+fi
+   
+if prompt "Source: lis, with 2 files (compare multiple fields)"; then
+   python autoviz.py -s lis -v 0
+   echo
+fi
+   
 #echo "Source: 'ccm' vs 'omi' "
 #python autoviz.py -s ccm,omi -v 0
 
@@ -207,17 +201,17 @@ echo
 
 # Altair backend
 
-c_option=/Users/ccruz/projects/Eviz/config
-echo "Source: 'gridded' - altair backend"
-python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_altair.yaml -v 0
-echo
-
+if prompt "Source: 'gridded' - altair backend"; then
+   python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_altair.yaml -v 0
+   echo
+fi
+   
 # Hvplot backend
 
-c_option=/Users/ccruz/projects/Eviz/config
-echo "Source: 'gridded' - hvplot backend"
-python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_hvplot.yaml -v 0
-echo
+if prompt "Source: 'gridded' - hvplot backend"; then
+   python autoviz.py -s gridded -f $EVIZ_CONFIG_PATH/gridded/gridded_hvplot.yaml -v 0
+   echo
+fi
 
 
 echo "End of tests"
