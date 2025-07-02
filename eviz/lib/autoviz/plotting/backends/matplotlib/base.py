Fix units and (minor) colorbar issues

eviz/lib/autoviz/plotting/backends/matplotlib/base.py
@@ -121,7 +121,6 @@ class MatplotlibBasePlotter(BasePlotter):
         if 'clevs' in ax_opts and ax_opts['clevs'] is not None and len(ax_opts['clevs']) > 0:
             return
         
-        # Check if data is all NaN
         if np.isnan(data2d).all():
             self.logger.warning("All values are NaN! Cannot create contour levels.")
             # Set default contour levels to avoid errors
@@ -129,7 +128,6 @@ class MatplotlibBasePlotter(BasePlotter):
             ax_opts['clevs_prec'] = 0
             return
         
-        # Use provided vmin and vmax if available
         if vmin is not None and vmax is not None:
             dmin, dmax = vmin, vmax
         else:
@@ -228,8 +226,6 @@ class MatplotlibBasePlotter(BasePlotter):
             if ax_opts.get("suppress_colorbar", False):
                 return None
 
-            source_name = config.source_names[config.ds_index]
-
             # Create formatter for colorbar ticks
             if ax_opts["cbar_sci_notation"]:
                 fmt = pu.FlexibleOOMFormatter(
@@ -256,9 +252,8 @@ class MatplotlibBasePlotter(BasePlotter):
                     shrink=pu.cbar_shrink(fig.subplots),
                 )
 
-            units = self.get_units(config, field_name, data2d, source_name, findex)
             if ax_opts["clabel"] is None:
-                cbar_label = units
+                cbar_label = self.units
             else:
                 cbar_label = ax_opts["clabel"]
             self.style_colorbar(cbar, ax_opts, data2d,
@@ -268,11 +263,6 @@ class MatplotlibBasePlotter(BasePlotter):
                 t.set_fontsize(pu.contour_tick_font_size(fig.subplots))
             for t in cbar.ax.get_yticklabels():
                 t.set_fontsize(pu.contour_tick_font_size(fig.subplots))
-
-            # Use consistent number of ticks
-            from matplotlib.ticker import MaxNLocator
-            cbar.locator = MaxNLocator(nbins=10)
-            cbar.update_ticks()
 
             return cbar
 
@@ -306,7 +296,6 @@ class MatplotlibBasePlotter(BasePlotter):
             self.logger.warning("No valid contours for shared colorbar")
             return None
         
-        # Log the number of valid contours and their limits
         self.logger.debug(f"Found {len(valid_contours)} valid contours for shared colorbar")
         for i, contour in enumerate(valid_contours):
             self.logger.debug(f"Contour {i} clim: {contour.get_clim()}")
@@ -321,29 +310,17 @@ class MatplotlibBasePlotter(BasePlotter):
         colorbar_width = getattr(config, 'colorbar_width', 0.02)
         cbar_ax = fig.add_axes([0.92, 0.15, colorbar_width, 0.7])
         
-        # Store reference to the colorbar axes
         fig._shared_colorbar_ax = cbar_ax
         
         # Create the colorbar using the first valid contour
         cbar = fig.colorbar(valid_contours[0], cax=cbar_ax)
         
-        # IMPORTANT: Update the colorbar limits to encompass all data for this field
+        # Encompass all data for this field
         cbar.mappable.set_clim(vmin, vmax)
         
-        # Improve tick label formatting
-        from matplotlib.ticker import MaxNLocator
-        cbar.locator = MaxNLocator(nbins=6)
-        cbar.update_ticks()
-        
-        # Set consistent tick font size
         tick_font_size = 8  # Fixed size for shared colorbar
         cbar.ax.tick_params(labelsize=tick_font_size)
-        
-        # Add units if available
-        if hasattr(config, 'spec_data') and field_name in config.spec_data:
-            if 'units' in config.spec_data[field_name]:
-                units = config.spec_data[field_name]['units']
-                cbar.set_label(units, size=10)
+        cbar.set_label(self.units, size=10)
         
         return cbar
 
@@ -351,7 +328,7 @@ class MatplotlibBasePlotter(BasePlotter):
     def set_const_colorbar(cfilled, fig, ax):
         _ = fig.colorbar(cfilled, ax=ax, shrink=0.5)
 
-    def get_units(self, config, field_name, data2d, source_name, findex):
+    def get_units(self, config, field_name, data2d, findex):
         """Get units for the field."""
         self.logger.debug(f"Get units for {field_name}")
         try:
@@ -368,15 +345,15 @@ class MatplotlibBasePlotter(BasePlotter):
 
             # Try to get units from the reader
             reader = None
-            if source_name in config.readers:
-                if isinstance(config.readers[source_name], dict):
-                    readers_dict = config.readers[source_name]
+            if self.source_name in config.readers:
+                if isinstance(config.readers[self.source_name], dict):
+                    readers_dict = config.readers[self.source_name]
                     if "NetCDF" in readers_dict:
                         reader = readers_dict["NetCDF"]
                     elif readers_dict:
                         reader = next(iter(readers_dict.values()))
                 else:
-                    reader = config.readers[source_name]
+                    reader = config.readers[self.source_name]
 
             if reader and hasattr(reader, "datasets"):
                 if findex in reader.datasets and "vars" in reader.datasets[findex]:

eviz/lib/autoviz/plotting/backends/matplotlib/tx_plot.py
@@ -29,7 +29,12 @@ class MatplotlibTXPlotter(MatplotlibBasePlotter):
         data2d, _, _, field_name, plot_type, findex, fig = data_to_plot
         if data2d is None:
             return fig
-        
+
+        self.source_name = config.source_names[config.ds_index]
+        self.units = self.get_units(config, 
+                                    field_name, 
+                                    data2d, 
+                                    findex)
         self.fig = fig
         
         ax_opts = config.ax_opts
@@ -106,8 +111,7 @@ class MatplotlibTXPlotter(MatplotlibBasePlotter):
             cbar = fig.colorbar(cfilled, orientation='horizontal', pad=0.1, aspect=70,
                                 extendrect=True)
             
-            units = self._get_units_for_colorbar(data2d)
-            cbar.set_label(units)
+            cbar.set_label(self.units)
             
             self._set_longitude_ticks(ax[1], lons)
             
@@ -265,17 +269,7 @@ class MatplotlibTXPlotter(MatplotlibBasePlotter):
                 lons = np.arange(data2d_reduced.shape[1])
         
         return data2d_reduced
-    
-    @staticmethod
-    def _get_units_for_colorbar(data2d):
-        """Get units for the colorbar."""
-        units = "n.a."
-        if hasattr(data2d, 'attrs') and 'units' in data2d.attrs:
-            units = data2d.attrs['units']
-        elif hasattr(data2d, 'units'):
-            units = data2d.units
-        return units
-    
+        
     @staticmethod
     def _setup_map_panel(ax):
         """Set up the map panel at the top of the Hovmoller plot."""

eviz/lib/autoviz/plotting/backends/matplotlib/yz_plot.py
@@ -25,6 +25,15 @@ class MatplotlibYZPlotter(MatplotlibBasePlotter):
             The created figure
         """
         data2d, x, y, field_name, plot_type, findex, fig = data_to_plot
+
+        if data2d is None:
+            return fig
+
+        self.source_name = config.source_names[config.ds_index]
+        self.units = self.get_units(config, 
+                                    field_name, 
+                                    data2d, 
+                                    findex)
         self.fig = fig
 
         ax_opts = config.ax_opts
@@ -57,43 +66,15 @@ class MatplotlibYZPlotter(MatplotlibBasePlotter):
         else:
             ax = ax_temp[0]
 
-        if data2d is None:
-            return
-
         ax_opts = fig.update_ax_opts(field_name, ax, 'yz')
         fig.plot_text(field_name, ax, 'yz', level=None, data=data2d)
 
         # Determine the vertical coordinate and its units
         zc = config.get_model_dim_name('zc')
-        vertical_coord = None
-        vertical_units = 'n.a.'
-
-        if isinstance(zc, dict):
-            for dim_name in data2d.coords:
-                if 'lev' in dim_name or 'z' in dim_name or 'height' in dim_name:
-                    vertical_coord = data2d.coords[dim_name]
-                    break
-        elif isinstance(zc, list):
-            for z in zc:
-                if z in data2d.coords:
-                    vertical_coord = data2d.coords[z]
-                    break
-        else:
-            if zc and zc in data2d.coords:
-                vertical_coord = data2d.coords[zc]
-            else:
-                # Try common vertical dimension names
-                for dim_name in ['lev', 'level', 'z', 'height', 'altitude', 'pressure']:
-                    if dim_name in data2d.coords:
-                        vertical_coord = data2d.coords[dim_name]
-                        break
-
-        if vertical_coord is not None:
-            vertical_units = vertical_coord.attrs.get('units', 'n.a.')
-        else:
-            if len(data2d.shape) >= 2:
-                # Assume first dimension is vertical in a YZ plot
-                vertical_coord = np.arange(data2d.shape[0])
+        try:
+            vertical_units = data2d.coords[zc].attrs['units']
+        except KeyError:
+            vertical_units = 'n.a.'
 
         if isinstance(ax, list):
             for single_ax in ax:
@@ -182,19 +163,8 @@ class MatplotlibYZPlotter(MatplotlibBasePlotter):
                         frame.set_alpha(0.7)
                         frame.set_edgecolor('gray')
 
-                # TODO: Fix units issue (missing sometimes)
-                if 'units' in config.spec_data[field_name]:
-                    units = config.spec_data[field_name]['units']
-                else:
-                    try:
-                        units = data2d.attrs['units']
-                        if units == '':
-                            units = "n.a."
-                    except Exception as e:
-                        self.logger.error(f"{e}: Please specify {field_name} units in specs file")
-                        units = "n.a."
                 if dep_var == 'zc':
-                    ax.set_xlabel(units)
+                    ax.set_xlabel(self.units)
                     ax.set_ylabel("Pressure (" + vertical_units + ")",
                                 size=pu.axes_label_font_size(fig.subplots))
 